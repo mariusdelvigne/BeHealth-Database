dfdecc81c79194f36cc7cbfdbfecbdc15f88df84
Marius Delvigne <marius.delvigne@gmail.com>
2024-11-13 10:53:14 +0100

    Mise en page avec les Foreign Key en dessous de chaque tables

--- Db/init.sql
+++ Db/init.sql
@@ -22,6 +22,10 @@ CREATE TABLE user_preferences (
     PRIMARY KEY (user_id)
 );
 GO
+ALTER TABLE user_preferences
+ADD CONSTRAINT fk_user_preferences_users_user_id
+FOREIGN KEY (user_id) REFERENCES users (user_id);
+GO
 CREATE TABLE global_messages (
     message_id INT NOT NULL IDENTITY,
     message_description VARCHAR(511) NOT NULL,
@@ -33,6 +37,10 @@ CREATE TABLE global_messages (
     PRIMARY KEY (message_id)
 );
 GO
+ALTER TABLE global_messages
+ADD CONSTRAINT fk_global_messages_users_creator_id
+FOREIGN KEY (creator_id) REFERENCES users (user_id);
+GO
 CREATE TABLE user_sleeps (
     sleep_id INT NOT NULL IDENTITY,
     sleep_start_datetime DATETIME2 NOT NULL,
@@ -42,6 +50,10 @@ CREATE TABLE user_sleeps (
 
     PRIMARY KEY (sleep_id)
 );
+GO
+ALTER TABLE user_sleeps
+ADD CONSTRAINT fk_user_sleeps_users_user_id
+FOREIGN KEY (user_id) REFERENCES users (user_id);
 GO
 CREATE TABLE user_periods (
     period_id INT NOT NULL IDENTITY,
@@ -53,6 +65,10 @@ CREATE TABLE user_periods (
     PRIMARY KEY (period_id)
 );
 GO
+ALTER TABLE user_periods
+ADD CONSTRAINT fk_user_periods_users_user_id
+FOREIGN KEY (user_id) REFERENCES users (user_id);
+GO
 CREATE TABLE user_weights (
     weight_id INT NOT NULL IDENTITY,
     weight_in_g INT NOT NULL,
@@ -62,6 +78,10 @@ CREATE TABLE user_weights (
 
     PRIMARY KEY (weight_id)
 );
+GO
+ALTER TABLE user_weights
+ADD CONSTRAINT fk_user_weights_users_user_id
+FOREIGN KEY (user_id) REFERENCES users (user_id);
 GO
 CREATE TABLE user_foods (
     food_id INT NOT NULL IDENTITY,
@@ -74,6 +94,10 @@ CREATE TABLE user_foods (
     PRIMARY KEY (food_id)
 );
 GO
+ALTER TABLE user_foods
+ADD CONSTRAINT fk_user_foods_users_user_id
+FOREIGN KEY (user_id) REFERENCES users (user_id);
+GO
 CREATE TABLE user_heights (
     height_id INT NOT NULL IDENTITY,
     height_in_cm INT NOT NULL,
@@ -83,6 +107,10 @@ CREATE TABLE user_heights (
 
     PRIMARY KEY (height_id)
 );
+GO
+ALTER TABLE user_heights
+ADD CONSTRAINT fk_user_heights_users_user_id
+FOREIGN KEY (user_id) REFERENCES users (user_id);
 GO
 CREATE TABLE user_sports (
     sport_id INT NOT NULL IDENTITY,
@@ -95,6 +123,10 @@ CREATE TABLE user_sports (
     PRIMARY KEY (sport_id)
 );
 GO
+ALTER TABLE user_sports
+ADD CONSTRAINT fk_user_sports_users_user_id
+FOREIGN KEY (user_id) REFERENCES users (user_id);
+GO
 CREATE TABLE user_passwords (
     password_id INT NOT NULL IDENTITY,
     password_password BINARY(60) NOT NULL,
@@ -104,6 +136,10 @@ CREATE TABLE user_passwords (
 
     PRIMARY KEY (password_id)
 );
+GO
+ALTER TABLE user_passwords
+ADD CONSTRAINT fk_user_passwords_users_user_id
+FOREIGN KEY (user_id) REFERENCES users (user_id);
 GO
 CREATE TABLE notifications (
     notification_id INT NOT NULL IDENTITY,
@@ -121,42 +157,6 @@ GO
 ALTER TABLE notifications
 ADD CONSTRAINT fk_notifications_users_user_id
 FOREIGN KEY (user_id) REFERENCES users (user_id);
-GO
-ALTER TABLE user_preferences
-ADD CONSTRAINT fk_user_preferences_users_user_id
-FOREIGN KEY (user_id) REFERENCES users (user_id);
-GO
-ALTER TABLE global_messages
-ADD CONSTRAINT fk_global_messages_users_creator_id
-FOREIGN KEY (creator_id) REFERENCES users (user_id);
-GO
-ALTER TABLE user_sleeps
-ADD CONSTRAINT fk_user_sleeps_users_user_id
-FOREIGN KEY (user_id) REFERENCES users (user_id);
-GO
-ALTER TABLE user_periods
-ADD CONSTRAINT fk_user_periods_users_user_id
-FOREIGN KEY (user_id) REFERENCES users (user_id);
-GO
-ALTER TABLE user_weights
-ADD CONSTRAINT fk_user_weights_users_user_id
-FOREIGN KEY (user_id) REFERENCES users (user_id);
-GO
-ALTER TABLE user_foods
-ADD CONSTRAINT fk_user_foods_users_user_id
-FOREIGN KEY (user_id) REFERENCES users (user_id);
-GO
-ALTER TABLE user_heights
-ADD CONSTRAINT fk_user_heights_users_user_id
-FOREIGN KEY (user_id) REFERENCES users (user_id);
-GO
-ALTER TABLE user_sports
-ADD CONSTRAINT fk_user_sports_users_user_id
-FOREIGN KEY (user_id) REFERENCES users (user_id);
-GO
-ALTER TABLE user_passwords
-ADD CONSTRAINT fk_user_passwords_users_user_id
-FOREIGN KEY (user_id) REFERENCES users (user_id);
 ---------------------------------------------------------------------------
 GO
 CREATE TABLE sleeps (
@@ -167,6 +167,10 @@ CREATE TABLE sleeps (
     PRIMARY KEY (plan_id)
 );
 GO
+ALTER TABLE sleeps
+ADD CONSTRAINT fk_sleeps_plans_plan_id
+FOREIGN KEY (plan_id) REFERENCES plans (plan_id);
+GO
 CREATE TABLE sports (
     sport_id INT NOT NULL IDENTITY,
     sport_name VARCHAR(127) NOT NULL UNIQUE,
@@ -176,6 +180,10 @@ CREATE TABLE sports (
     PRIMARY KEY (sport_id)
 );
 GO
+ALTER TABLE sports
+ADD CONSTRAINT fk_sports_users_creator_id
+FOREIGN KEY (creator_id) REFERENCES users (user_id);
+GO
 CREATE TABLE foods (
     food_id INT NOT NULL IDENTITY,
     food_name VARCHAR(127) NOT NULL UNIQUE,
@@ -184,6 +192,10 @@ CREATE TABLE foods (
 
     PRIMARY KEY (food_id)
 );
+GO
+ALTER TABLE foods
+ADD CONSTRAINT fk_foods_users_creator_id
+FOREIGN KEY (creator_id) REFERENCES users (user_id);
 GO
 CREATE TABLE plans (
     plan_id INT NOT NULL IDENTITY,
@@ -199,6 +211,10 @@ CREATE TABLE plans (
     PRIMARY KEY (plan_id)
 );
 GO
+ALTER TABLE plans
+ADD CONSTRAINT fk_plans_users_creator_id
+FOREIGN KEY (creator_id) REFERENCES users (user_id);
+GO
 CREATE TABLE ast_plans_foods (
     ast_id INT NOT NULL IDENTITY,
     ast_day_number INT NOT NULL,
@@ -209,6 +225,14 @@ CREATE TABLE ast_plans_foods (
 
     PRIMARY KEY (ast_id)
 );
+GO
+ALTER TABLE ast_plans_foods
+ADD CONSTRAINT fk_ast_plans_foods_foods_food_id
+FOREIGN KEY (food_id) REFERENCES foods (food_id);
+GO
+ALTER TABLE ast_plans_foods
+ADD CONSTRAINT fk_ast_plans_foods_plans_plan_id
+FOREIGN KEY (plan_id) REFERENCES plans (plan_id);
 GO
 CREATE TABLE ast_plans_sports (
     ast_id INT NOT NULL IDENTITY,
@@ -221,6 +245,14 @@ CREATE TABLE ast_plans_sports (
     PRIMARY KEY (ast_id)
 );
 GO
+ALTER TABLE ast_plans_sports
+ADD CONSTRAINT fk_ast_plans_sports_sports_sport_id
+FOREIGN KEY (sport_id) REFERENCES sports (sport_id);
+GO
+ALTER TABLE ast_plans_sports
+ADD CONSTRAINT fk_ast_plans_sports_plans_plan_id
+FOREIGN KEY (plan_id) REFERENCES plans (plan_id);
+GO
 CREATE TABLE tags (
     tag_id INT NOT NULL IDENTITY,
     tag_name VARCHAR(63) NOT NULL,
@@ -230,6 +262,10 @@ CREATE TABLE tags (
     PRIMARY KEY (tag_id)
 );
 GO
+ALTER TABLE tags
+ADD CONSTRAINT fk_tags_users_creator_id
+FOREIGN KEY (creator_id) REFERENCES users (user_id);
+GO
 CREATE TABLE ast_plans_tags (
     ast_id INT NOT NULL IDENTITY,
 
@@ -238,6 +274,14 @@ CREATE TABLE ast_plans_tags (
 
     PRIMARY KEY (ast_id)
 );
+GO
+ALTER TABLE ast_plans_tags
+ADD CONSTRAINT fk_ast_plans_tags_plans_plan_id
+FOREIGN KEY (plan_id) REFERENCES plans (plan_id);
+GO
+ALTER TABLE ast_plans_tags
+ADD CONSTRAINT fk_ast_plans_tags_tags_tag_id
+FOREIGN KEY (tag_id) REFERENCES tags (tag_id);
 GO
 CREATE TABLE health_programs (
     program_id INT NOT NULL IDENTITY,
@@ -254,6 +298,22 @@ CREATE TABLE health_programs (
     PRIMARY KEY (program_id)
 );
 GO
+ALTER TABLE health_programs
+ADD CONSTRAINT fk_health_programs_plans_sleep_plan_id
+FOREIGN KEY (sleep_plan_id) REFERENCES plans (plan_id);
+GO
+ALTER TABLE health_programs
+ADD CONSTRAINT fk_health_programs_plans_food_plan_id
+FOREIGN KEY (food_plan_id) REFERENCES plans (plan_id);
+GO
+ALTER TABLE health_programs
+ADD CONSTRAINT fk_health_programs_plans_sport_plan_id
+FOREIGN KEY (sport_plan_id) REFERENCES plans (plan_id);
+GO
+ALTER TABLE health_programs
+ADD CONSTRAINT fk_health_programs_users_creator_id
+FOREIGN KEY (creator_id) REFERENCES users (user_id);
+GO
 CREATE TABLE ast_favorite_plans_users (
     ast_id INT NOT NULL IDENTITY,
 
@@ -263,6 +323,14 @@ CREATE TABLE ast_favorite_plans_users (
     PRIMARY KEY (ast_id)
 );
 GO
+ALTER TABLE ast_favorite_plans_users
+ADD CONSTRAINT fk_ast_favorite_plans_users_users_user_id
+FOREIGN KEY (user_id) REFERENCES users (user_id);
+GO
+ALTER TABLE ast_favorite_plans_users
+ADD CONSTRAINT fk_ast_favorite_plans_users_plans_plan_id
+FOREIGN KEY (plan_id) REFERENCES plans (plan_id);
+GO
 CREATE TABLE ast_subscription_plans_users (
     ast_id INT NOT NULL IDENTITY,
 
@@ -271,6 +339,14 @@ CREATE TABLE ast_subscription_plans_users (
 
     PRIMARY KEY (ast_id)
 );
+GO
+ALTER TABLE ast_subscription_plans_users
+ADD CONSTRAINT fk_ast_subscription_plans_users_users_user_id
+FOREIGN KEY (user_id) REFERENCES users (user_id);
+GO
+ALTER TABLE ast_subscription_plans_users
+ADD CONSTRAINT fk_ast_subscription_plans_users_plans_plan_id
+FOREIGN KEY (plan_id) REFERENCES plans (plan_id);
 GO
 CREATE TABLE program_feedbacks (
     feedback_id INT NOT NULL IDENTITY,
@@ -284,62 +360,6 @@ CREATE TABLE program_feedbacks (
     PRIMARY KEY (feedback_id)
 );
 GO
-ALTER TABLE sports
-ADD CONSTRAINT fk_sports_users_creator_id
-FOREIGN KEY (creator_id) REFERENCES users (user_id);
-GO
-ALTER TABLE foods
-ADD CONSTRAINT fk_foods_users_creator_id
-FOREIGN KEY (creator_id) REFERENCES users (user_id);
-GO
-ALTER TABLE sleeps
-ADD CONSTRAINT fk_sleeps_plans_plan_id
-FOREIGN KEY (plan_id) REFERENCES plans (plan_id);
-GO
-ALTER TABLE ast_plans_foods
-ADD CONSTRAINT fk_ast_plans_foods_foods_food_id
-FOREIGN KEY (food_id) REFERENCES foods (food_id);
-GO
-ALTER TABLE ast_plans_foods
-ADD CONSTRAINT fk_ast_plans_foods_plans_plan_id
-FOREIGN KEY (plan_id) REFERENCES plans (plan_id);
-GO
-ALTER TABLE ast_plans_sports
-ADD CONSTRAINT fk_ast_plans_sports_sports_sport_id
-FOREIGN KEY (sport_id) REFERENCES sports (sport_id);
-GO
-ALTER TABLE ast_plans_sports
-ADD CONSTRAINT fk_ast_plans_sports_plans_plan_id
-FOREIGN KEY (plan_id) REFERENCES plans (plan_id);
-GO
-ALTER TABLE health_programs
-ADD CONSTRAINT fk_health_programs_plans_sleep_plan_id
-FOREIGN KEY (sleep_plan_id) REFERENCES plans (plan_id);
-GO
-ALTER TABLE health_programs
-ADD CONSTRAINT fk_health_programs_plans_food_plan_id
-FOREIGN KEY (food_plan_id) REFERENCES plans (plan_id);
-GO
-ALTER TABLE health_programs
-ADD CONSTRAINT fk_health_programs_plans_sport_plan_id
-FOREIGN KEY (sport_plan_id) REFERENCES plans (plan_id);
-GO
-ALTER TABLE ast_plans_tags
-ADD CONSTRAINT fk_ast_plans_tags_plans_plan_id
-FOREIGN KEY (plan_id) REFERENCES plans (plan_id);
-GO
-ALTER TABLE ast_plans_tags
-ADD CONSTRAINT fk_ast_plans_tags_tags_tag_id
-FOREIGN KEY (tag_id) REFERENCES tags (tag_id);
-GO
-ALTER TABLE tags
-ADD CONSTRAINT fk_tags_users_creator_id
-FOREIGN KEY (creator_id) REFERENCES users (user_id);
-GO
-ALTER TABLE plans
-ADD CONSTRAINT fk_plans_users_creator_id
-FOREIGN KEY (creator_id) REFERENCES users (user_id);
-GO
 ALTER TABLE program_feedbacks
 ADD CONSTRAINT fk_program_feedbacks_users_creator_id
 FOREIGN KEY (creator_id) REFERENCES users (user_id);
@@ -347,26 +367,6 @@ GO
 ALTER TABLE program_feedbacks
 ADD CONSTRAINT fk_program_feedbacks_health_programs_program_id
 FOREIGN KEY (program_id) REFERENCES health_programs (program_id);
-GO
-ALTER TABLE health_programs
-ADD CONSTRAINT fk_health_programs_users_creator_id
-FOREIGN KEY (creator_id) REFERENCES users (user_id);
-GO
-ALTER TABLE ast_subscription_plans_users
-ADD CONSTRAINT fk_ast_subscription_plans_users_users_user_id
-FOREIGN KEY (user_id) REFERENCES users (user_id);
-GO
-ALTER TABLE ast_subscription_plans_users
-ADD CONSTRAINT fk_ast_subscription_plans_users_plans_plan_id
-FOREIGN KEY (plan_id) REFERENCES plans (plan_id);
-GO
-ALTER TABLE ast_favorite_plans_users
-ADD CONSTRAINT fk_ast_favorite_plans_users_users_user_id
-FOREIGN KEY (user_id) REFERENCES users (user_id);
-GO
-ALTER TABLE ast_favorite_plans_users
-ADD CONSTRAINT fk_ast_favorite_plans_users_plans_plan_id
-FOREIGN KEY (plan_id) REFERENCES plans (plan_id);
 GO
 INSERT INTO users (user_role, User_mail, user_username, user_birth_date, user_gender, user_name, user_surname) VALUES (
     'Admin', 'test@mail.com', 'test', GETDATE(), 'Male', 'test', 'test'
